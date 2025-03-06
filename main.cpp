2aedd4a58a567fdb5e9d5d666ac6dffafb49f897
MobinaEbrahimi <mobina.pourmehdi85@gmail.com>
2025-03-06 13:21:21 +0330

    adding information

--- main.cpp
+++ main.cpp
@@ -2,5 +2,8 @@
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "Mobina Pourmehdi Ebrahimi" << std::endl;
+    std::cout << "403101615" << std::endl;
+
     return 0;
 }
